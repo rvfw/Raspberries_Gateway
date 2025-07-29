4881880f911910bd23ffd05781ec630956b3da9b
rvfw <k.daniil2506@gmail.com>
2025-07-29 21:03:00 +0400

    Added load balancer

--- src/main/java/com/example/raspberriesgateway/config/GatewayConfig.java
+++ src/main/java/com/example/raspberriesgateway/config/GatewayConfig.java
@@ -1,5 +1,7 @@
 package com.example.raspberriesGateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,5 +19,16 @@ public class GatewayConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsWebFilter(source);
+    }
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service",r->r.path("/api/auth/**")
+                        .uri("lb://auth-service"))
+                .route("user-service",r->r.path("/api/user/**","/api/users")
+                        .uri("lb://user-service"))
+                .route("seller-service",r->r.path("/api/seller/**","/api/sellers")
+                        .uri("lb://seller-service"))
+                .build();
     }
 }
